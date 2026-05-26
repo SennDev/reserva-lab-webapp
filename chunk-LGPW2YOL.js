d5f2292dfd9540da024f8da57eb54aafe1f673d1
SennDev <GersonEM8900@outlook.com>
2026-05-26 02:39:38 -0600

    Auto-generated commit

---
+++ chunk-LGPW2YOL.js
@@ -0,0 +1,1 @@
+import{F as n}from"./chunk-TG3DUXJJ.js";var o="http://127.0.0.1:8000",i=new n("API_BASE_URL",{factory:()=>o});function e(t){return t.endsWith("/")?t.slice(0,-1):t}export{o as a,i as b,e as c};
